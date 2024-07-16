a03732c06098557cd42ee916de908be41a716aa2
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-07-16 19:36:51 +0900

    competed Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Icons } from "./Icons"
 import NavItems from "./NavItems"
+import { buttonVariants } from "./ui/button"
 
 
 const Navbar = () => {
+
+    const user = null
 
 
     return (
@@ -22,6 +25,36 @@ const Navbar = () => {
                             <div className="hidden z-10 lg:ml-8 lg:block lg:self-stretch">
                                 <NavItems />
                             </div>
+                            <div
+                                className="ml-auto flex items-center"
+                            >
+                                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                                    {
+                                        user ? null : (<Link href='/sign-in' className={buttonVariants({variant: 'ghost'})}> Sign in</Link>)
+                                    }
+                                    {
+                                        user ? null : <span className="h-6 w-px bg-gray-200" aria-hidden="true"/>
+                                    }
+                                    {
+                                        user ? <p></p> : <Link href='/sign-up' className={buttonVariants({variant: 'ghost'})}>Create account </Link>
+                                    }
+
+                                    {
+                                        user ? <span className="h-6 w-px bg-gray-200" aria-hidden="true"/> : null
+                                    }
+
+                                    { 
+                                        user ? null : <div className="flex lg:ml-6">
+                                            <span className="h-6 w-px bg-gray-200" aria-hidden="true"/> 
+                                        </div>
+                                    }
+
+                                    <div className="ml-4 flow-root lg:ml-6">
+                                        {/* <Cart /> */}
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </MaxWidthWrapper>
